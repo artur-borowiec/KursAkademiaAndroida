1bac2b0cfdfe8f2c0ed6b949364301acd60f4792
Artur <artur.borowiec0@interia.pl>
2020-07-15 11:41:25 +0200

    Database module added

---
+++ app/src/main/java/pl/arturborowiec/kursakademiaandroida/core/di/DatabaseModule.kt
@@ -0,0 +1,18 @@
+package pl.arturborowiec.kursakademiaandroida.core.di
+
+import androidx.room.Room
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import pl.arturborowiec.kursakademiaandroida.core.feature.data.db.AppDatabase
+
+private const val DATABASE_NAME = "db"
+
+val databaseModule = module {
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            AppDatabase::class.java,
+            DATABASE_NAME
+        ).build()
+    }
+}
