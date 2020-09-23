e11981b8e6fb86f4fdd74c942e2f1d95e9e8eaad
Artur Borowiec <artur.borowiec0@interia.pl>
2020-09-23 20:20:13 +0200

    LiveEvent setup

--- app/src/main/java/pl/arturborowiec/kursakademiaandroida/features/episodes/presentation/EpisodeFragment.kt
+++ app/src/main/java/pl/arturborowiec/kursakademiaandroida/features/episodes/presentation/EpisodeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.observe
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -18,6 +19,17 @@ class EpisodeFragment : Fragment() {
         super.onCreate(savedInstanceState)
         observeEpisodes()
         observeUiState()
+        observeMessage()
+    }
+
+    private fun observeMessage() {
+        viewModel.message.observe(this) {
+            showToast(it)
+        }
+    }
+
+    private fun showToast(it: String) {
+        Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
     }
 
     private fun observeUiState() {
--- app/src/main/java/pl/arturborowiec/kursakademiaandroida/features/episodes/presentation/EpisodeViewModel.kt
+++ app/src/main/java/pl/arturborowiec/kursakademiaandroida/features/episodes/presentation/EpisodeViewModel.kt
@@ -1,12 +1,17 @@
 package pl.arturborowiec.kursakademiaandroida.features.episodes.presentation
 
 import androidx.lifecycle.*
+import com.hadilq.liveevent.LiveEvent
 import pl.arturborowiec.kursakademiaandroida.core.base.UIState
 import pl.arturborowiec.kursakademiaandroida.features.episodes.domain.GetEpisodesUseCase
 import pl.arturborowiec.kursakademiaandroida.features.episodes.domain.model.Episode
 import pl.arturborowiec.kursakademiaandroida.features.episodes.presentation.model.EpisodeDisplayable
 
 class EpisodeViewModel(private val getEpisodesUseCase: GetEpisodesUseCase) : ViewModel() {
+
+    private val _message by lazy { LiveEvent<String>() }
+
+    val message: LiveData<String> by lazy { _message }
 
     private val _uiState by lazy { MutableLiveData<UIState>(UIState.Idle) }
 
@@ -34,8 +39,15 @@ class EpisodeViewModel(private val getEpisodesUseCase: GetEpisodesUseCase) : Vie
                 episodeLiveData.value = episodes
             }
 
-            result.onFailure {}
+            result.onFailure { throwable ->
+                throwable.message
+                    ?.let { showMessage(it) }
+            }
         }
+    }
+
+    private fun showMessage(message: String) {
+        _message.value = message
     }
 
     private fun setIdleState() {
