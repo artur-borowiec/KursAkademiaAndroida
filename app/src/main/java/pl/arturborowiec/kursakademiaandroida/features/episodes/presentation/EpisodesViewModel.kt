c116587e7bed64f783c585ded4891e9b66d853d9
Artur Borowiec <artur.borowiec0@interia.pl>
2020-09-23 16:33:27 +0200

    UseCase invoke

---
+++ app/src/main/java/pl/arturborowiec/kursakademiaandroida/features/episodes/presentation/EpisodesViewModel.kt
@@ -0,0 +1,28 @@
+package pl.arturborowiec.kursakademiaandroida.features.episodes.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import pl.arturborowiec.kursakademiaandroida.features.episodes.domain.GetEpisodesUseCase
+import pl.arturborowiec.kursakademiaandroida.features.episodes.presentation.model.EpisodeDisplayable
+
+class EpisodesViewModel(private val getEpisodesUseCase: GetEpisodesUseCase) : ViewModel() {
+
+    val episodes by lazy {
+        MutableLiveData<List<EpisodeDisplayable>>()
+            .also { getEpisodes(it) }
+    }
+
+    private fun getEpisodes(episodeLiveData: MutableLiveData<List<EpisodeDisplayable>>) {
+        getEpisodesUseCase(
+            params = Unit,
+            scope = viewModelScope
+        ) { result ->
+            result.onSuccess { episodes ->
+                episodeLiveData.value = episodes.map { EpisodeDisplayable(it) }
+            }
+
+            result.onFailure {}
+        }
+    }
+}
