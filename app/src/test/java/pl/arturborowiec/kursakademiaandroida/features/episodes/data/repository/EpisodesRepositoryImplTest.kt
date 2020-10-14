7f629405a64f78d18f520fce0b787b00271d6366
Artur <artur.borowiec0@interia.pl>
2020-10-14 21:29:59 +0200

    Tests corrected after error handling implementation

--- app/src/test/java/pl/arturborowiec/kursakademiaandroida/features/episodes/data/repository/EpisodesRepositoryImplTest.kt
+++ app/src/test/java/pl/arturborowiec/kursakademiaandroida/features/episodes/data/repository/EpisodesRepositoryImplTest.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import pl.arturborowiec.kursakademiaandroida.core.api.RickAndMortyApi
 import pl.arturborowiec.kursakademiaandroida.core.api.model.EpisodesResponse
+import pl.arturborowiec.kursakademiaandroida.core.exception.ErrorWrapper
 import pl.arturborowiec.kursakademiaandroida.core.network.NetworkStateProvider
 import pl.arturborowiec.kursakademiaandroida.features.episodes.data.local.EpisodeDao
 import pl.arturborowiec.kursakademiaandroida.features.episodes.data.local.model.EpisodeCached
@@ -26,9 +27,10 @@ internal class EpisodesRepositoryImplTest {
         val networkStateProvider = mockk<NetworkStateProvider> {
             every { isNetworkAvailable() } returns true
         }
+        val errorWrapper = mockk<ErrorWrapper>(relaxed = true)
 
         val repository: EpisodesRepository =
-            EpisodesRepositoryImpl(api, episodeDao, networkStateProvider)
+            EpisodesRepositoryImpl(api, episodeDao, networkStateProvider, errorWrapper)
 
         // when
         runBlocking { repository.getEpisodes() }
@@ -47,9 +49,10 @@ internal class EpisodesRepositoryImplTest {
         val networkStateProvider = mockk<NetworkStateProvider> {
             every { isNetworkAvailable() } returns true
         }
+        val errorWrapper = mockk<ErrorWrapper>(relaxed = true)
 
         val repository: EpisodesRepository =
-            EpisodesRepositoryImpl(api, episodeDao, networkStateProvider)
+            EpisodesRepositoryImpl(api, episodeDao, networkStateProvider, errorWrapper)
 
         // when
         runBlocking { repository.getEpisodes() }
@@ -68,9 +71,10 @@ internal class EpisodesRepositoryImplTest {
         val networkStateProvider = mockk<NetworkStateProvider> {
             every { isNetworkAvailable() } returns false
         }
+        val errorWrapper = mockk<ErrorWrapper>(relaxed = true)
 
         val repository: EpisodesRepository =
-            EpisodesRepositoryImpl(api, episodeDao, networkStateProvider)
+            EpisodesRepositoryImpl(api, episodeDao, networkStateProvider, errorWrapper)
 
         // when
         runBlocking { repository.getEpisodes() }
--- app/src/test/java/pl/arturborowiec/kursakademiaandroida/features/locations/presentation/LocationViewModelTest.kt
+++ app/src/test/java/pl/arturborowiec/kursakademiaandroida/features/locations/presentation/LocationViewModelTest.kt
@@ -8,6 +8,7 @@ import io.mockk.verify
 import org.amshove.kluent.shouldBe
 import org.junit.jupiter.api.Test
 import pl.arturborowiec.kursakademiaandroida.core.base.UIState
+import pl.arturborowiec.kursakademiaandroida.core.exception.ErrorMapper
 import pl.arturborowiec.kursakademiaandroida.features.locations.domain.GetLocationsUseCase
 import pl.arturborowiec.kursakademiaandroida.features.locations.domain.model.Location
 import pl.arturborowiec.kursakademiaandroida.mock.mock
@@ -21,7 +22,8 @@ internal class LocationViewModelTest : ViewModelTest() {
     fun `WHEN location live data is observed THEN set pending state`() {
         // given
         val useCase = mockk<GetLocationsUseCase>(relaxed = true)
-        val viewModel = LocationViewModel(useCase)
+        val errorMapper = mockk<ErrorMapper>(relaxed = true)
+        val viewModel = LocationViewModel(useCase, errorMapper)
 
         // when
         viewModel.locations.observeForTesting()
@@ -34,7 +36,8 @@ internal class LocationViewModelTest : ViewModelTest() {
     fun `WHEN location live data is observed THEN invoke use case to get locations`() {
         // given
         val useCase = mockk<GetLocationsUseCase>(relaxed = true)
-        val viewModel = LocationViewModel(useCase)
+        val errorMapper = mockk<ErrorMapper>(relaxed = true)
+        val viewModel = LocationViewModel(useCase, errorMapper)
 
         // when
         viewModel.locations.observeForTesting()
@@ -52,7 +55,8 @@ internal class LocationViewModelTest : ViewModelTest() {
                 lastArg<(Result<List<Location>>) -> Unit>()(Result.success(locations))
             }
         }
-        val viewModel = LocationViewModel(useCase)
+        val errorMapper = mockk<ErrorMapper>(relaxed = true)
+        val viewModel = LocationViewModel(useCase, errorMapper)
 
         // when
         viewModel.locations.observeForTesting()
@@ -77,7 +81,10 @@ internal class LocationViewModelTest : ViewModelTest() {
             }
         }
         val observer = mockk<Observer<String>>(relaxed = true)
-        val viewModel = LocationViewModel(useCase)
+        val errorMapper = mockk<ErrorMapper> {
+            every { map(any()) } returns throwable.message!!
+        }
+        val viewModel = LocationViewModel(useCase, errorMapper)
 
         // when
         viewModel.message.observeForever(observer)
